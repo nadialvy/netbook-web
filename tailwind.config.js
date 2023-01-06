5e8a3cc527958cdc24f5f87cf0cb2c444bd04d9b
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-01-07 05:27:29 +0700

    Customize font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          "900" : "#0A093D"
+        },
+        darkBlue: {
+          '800' : "#2B2B39",
+          '900' : "#2B2B39"
+        },
+        tosca: {
+          "300" : "#E5F0FD"
+        }
+      }
+    },
   },
   plugins: [],
 }
